af8c6b474ae6bd0bd796e4723f3ac3ed6234ea05
Mudit Gandhi <mudit2752004@gmail.com>
2025-05-12 06:42:18 +0530

    initial-4

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App.jsx'
@@ -6,23 +6,24 @@ import { store } from './store/store';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from './components/ui/themeProvider';
-import { BrowserRouter } from 'react-router-dom';
+
 import { SidebarProvider } from './components/ui/sidebar';
+import { HashRouter } from 'react-router-dom';
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  
      <GoogleOAuthProvider clientId={clientId}>
     <Provider store={store}>
     <ThemeProvider>
-    <BrowserRouter>
+    <HashRouter>
     <SidebarProvider>
     <App />
     </SidebarProvider>
-    </BrowserRouter>
+    </HashRouter>
    </ThemeProvider>
   </Provider>
   </GoogleOAuthProvider>
-  </StrictMode>
+  
 )
